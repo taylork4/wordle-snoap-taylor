20b8df18827939f1e1235ea8e9fc806f75038c5f
Kyle Taylor <89995487+taylork4@users.noreply.github.com>
2023-02-17 09:18:44 -0500

    File Upload :)

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import HelloWordle from './components/HelloWordle.vue'
+</script>
+
+<template>
+  <h1>Welcome</h1>
+  <HelloWordle />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/HelloWordle.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref, Ref } from 'vue'
+const gameName = ref("Wordle Clone")
+const userWords: Ref<string[]> = ref([])
+
+function addOneWord() {
+  userWords.value.push("Hello")
+}
+
+function clearAll() {
+  userWords.value.splice(0)
+}
+</script>
+
+<template>
+  <p>Start your work of {{ gameName }} in this file</p>
+  <button @click="addOneWord">Add "Hello"</button>
+  <button @click="clearAll">Remove All</button>
+  <ul>
+    <li v-for="(w, pos) in userWords" v-bind:key="pos">{{ w }}</li>
+  </ul>
+</template>
+
+<style scoped>
+
+</style>
