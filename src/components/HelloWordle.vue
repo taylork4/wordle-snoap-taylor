a0a32027fdc1423c4064b7ce598cf930b6ffd0c7
Kyle Taylor <taylork4@mail.gvsu.edu>
2023-02-20 11:07:33 -0500

    Kyle Taylor - Color Logic implemented

--- src/components/HelloWordle.vue
+++ src/components/HelloWordle.vue
@@ -7,20 +7,22 @@ const userWords: Ref<string[]> = ref(["", "", "", "", "",
                                       "", "", "", "", "",
                                       "", "", "", "", "",
                                       "", "", "", "", "",
-                                      "", "", "", "", "",])
-const letterColor: Ref<number[]> = ref([0, 0, 0, 0, 0,
-                                        0, 0, 0, 0, 0,
-                                        0, 0, 0, 0, 0,
-                                        0, 0, 0, 0, 0,
-                                        0, 0, 0, 0, 0,
-                                        0, 0, 0, 0, 0,])
+                                      "", "", "", "", ""])
+const letterColor: Ref<string[]> = ref(["", "", "", "", "",
+                                      "", "", "", "", "",
+                                      "", "", "", "", "",
+                                      "", "", "", "", "",
+                                      "", "", "", "", "",
+                                      "", "", "", "", ""])
 let checks: number = 0
 let checksShow: boolean = true
 
 function newGame() {
   userWords.value.splice(0)
+  letterColor.value.splice(0)
   for (let i = 0; i < 30; i++) {
     userWords.value.push("");
+    letterColor.value.push("");
   }
 }
 
@@ -29,17 +31,14 @@ function checkAnswer() {
   for (let k = 0; k <= 25; k += 5) {
     for (let i = 0; i < 5; i++) {
       if (userWords.value[i + k] == "") {
-        userWords.value[i + k] = "" //correct letter wrong spot
+        userWords.value[i + k] = "" //Blank cell
       } else if (userWords.value[i + k].toLowerCase() == secretWord.charAt(i)) {
-        userWords.value[i + k] = "G" //correct letter in correct location
+        letterColor.value[i + k] = "G" //correct letter in correct location
       } else if (secretWord.includes(userWords.value[i + k].toLowerCase())){
-        userWords.value[i + k] = "Y" //correct letter wrong spot
+        letterColor.value[i + k] = "Y" //correct letter wrong spot
+      } else {
+        letterColor.value[i + k] = "B" //wrong letter
       }
-      else {
-        userWords.value[i + k] = "B" //wrong letter
-      }
-
-
     }
   } 
 }
@@ -54,11 +53,14 @@ function checkAnswer() {
       <input id = "cS2" v-show ="checksShow == false" v-model = "userWords[pos]"/> -->
 
         <!-- <input id = "cS2" v-if="checks == 1" v-model = "userWords[pos]"/> -->
-        <input id = "cS1" v-model = "userWords[pos]"/>
+        <input class = "cell" v-if="letterColor[pos] == ''" v-model = "userWords[pos]"/>
+        <input class = "cell" id = "wrong" v-else-if="letterColor[pos] == 'B'" v-model = "userWords[pos]"/>
+        <input class = "cell" id = "right" v-else-if="letterColor[pos] == 'G'" v-model = "userWords[pos]"/>
+        <input class = "cell" id = "misplaced" v-else-if="letterColor[pos] == 'Y'" v-model = "userWords[pos]"/>
 
     </p>
   </div>
-  <div id = "buttons">
+  <div class = "buttons">
     <button @click="newGame">New Game</button>
     <button @click="checkAnswer">Check Answer</button>
     <!-- <button @click="addOneWord">Check</button> -->
@@ -68,29 +70,13 @@ function checkAnswer() {
 <style scoped> 
 
   #grid {
-    /* display: grid; */
-    /* min-width: 100px; */
     display: inline-grid;
     grid-template-columns: repeat(5, 1fr);
     grid-template-rows: repeat(6, 1fr);  
     grid-gap: 8px;
-    
-    /* display: inline-grid;
-    grid-template-columns: repeat(5, 1fr);
-    font-size: 100px;
-    grid-gap: 20px; */
-    /* width: 0em; */
   }
 
-  #cell {
-    /* width: 50px;  */
-    /* font-size: 1em; */
-    /* font-weight: 500; */
-    
-
-  }
-
-  #cS1 {
+  .cell {
     text-transform: uppercase;
     text-align: center;
     width: 100px;
@@ -98,22 +84,27 @@ function checkAnswer() {
     align-self: center;
     font-size: 100px;
     border: 5px solid black;
-    /* background-color: blue; */
   }
 
-  /* #cS2 {
-    text-transform: uppercase;
-    text-align: center;
-    width: 100px;
-    height: 100px;
-    align-self: center;
-    font-size: 100px;
-    border: 5px solid black;
-    background-color: blue;
-  } */
+  #wrong {
+    background-color: grey;
+  }
 
-  #buttons {
-    /* display: grid; */
+  #right {
+    background-color: green;
+  }
+
+  #misplaced {
+    background-color: rgb(186, 186, 3);
+  }
+
+  .buttons {
+    /* width: auto; */
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    grid-gap: 8px;
+    /* display: grid-template-rows; */
     /* padding: 20px; */
   }
 </style>
