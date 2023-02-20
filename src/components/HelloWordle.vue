b5d03d342840b53c7ded4a6f5b7feee1f0996414
snoapca <snoapca@Ubuntu22.04.myguest.virtualbox.org>
2023-02-19 21:17:58 -0500

    changed the title of the page, added your name to the bottom, and have a start on the checkAnswer logic

--- src/components/HelloWordle.vue
+++ src/components/HelloWordle.vue
@@ -2,11 +2,11 @@
 import { ref, Ref } from 'vue'
 const gameName = ref("SnoTay Wordle")
 const userWords: Ref<string[]> = ref(["", "", "", "", "",
-                                      "", "", "", "", "",
-                                      "", "", "", "", "",
-                                      "", "", "", "", "",
-                                      "", "", "", "", "",
-                                      "", "", "", "", "",])
+  "", "", "", "", "",
+  "", "", "", "", "",
+  "", "", "", "", "",
+  "", "", "", "", "",
+  "", "", "", "", "",])
 
 function newGame() {
   userWords.value.splice(0)
@@ -15,49 +15,56 @@ function newGame() {
   }
 }
 
+function checkAnswer() {
+  const secretWord = "ALPIN" //test word
+  { } //pass
+}
+
 </script>
 
 <template>
   <p>Start your work of {{ gameName }} in this file</p>
   <div id="grid">
     <p v-for="(w, pos) in userWords" v-bind:key="pos">
-      <div id = "cell">
-        <input v-model = "userWords[pos]" style = "text-transform: uppercase; text-align: center; width: 100px; height: 100px; align-self: center; font-size: 100px; border: 5px solid black;" />
-      </div>
+    <div id="cell">
+      <input v-model="userWords[pos]"
+        style="text-transform: uppercase; text-align: center; width: 100px; height: 100px; align-self: center; font-size: 100px; border: 5px solid black;" />
+    </div>
     </p>
   </div>
-  <div id = "buttons">
+  <div id="buttons">
     <button @click="newGame">New Game</button>
+    <button @click="checkAnswer">Check Answer</button>
     <!-- <button @click="addOneWord">Check</button> -->
   </div>
 </template>
 
 <style scoped>
-  #grid {
-    /* display: grid; */
-    /* min-width: 100px; */
-    display: inline-grid;
-    grid-template-columns: repeat(5, 1fr);
-    grid-template-rows: repeat(6, 1fr);  
-    grid-gap: 8px;
-    
-    /* display: inline-grid;
+#grid {
+  /* display: grid; */
+  /* min-width: 100px; */
+  display: inline-grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: repeat(6, 1fr);
+  grid-gap: 8px;
+
+  /* display: inline-grid;
     grid-template-columns: repeat(5, 1fr);
     font-size: 100px;
     grid-gap: 20px; */
-    /* width: 0em; */
-  }
+  /* width: 0em; */
+}
 
-  #cell {
-    /* width: 50px;  */
-    /* font-size: 1em; */
-    /* font-weight: 500; */
-    
+#cell {
+  /* width: 50px;  */
+  /* font-size: 1em; */
+  /* font-weight: 500; */
 
-  }
 
-  #buttons {
-    /* display: grid; */
-    /* padding: 20px; */
-  }
+}
+
+#buttons {
+  /* display: grid; */
+  /* padding: 20px; */
+}
 </style>
