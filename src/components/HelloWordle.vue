83b89a8b3f973c4cbefa2fea7111ba92f0ae6441
snoapca <snoapca@Ubuntu22.04.myguest.virtualbox.org>
2023-03-15 11:46:53 -0400

    added firebase init and auth, error on line 231 needs to be fixed

--- src/components/HelloWordle.vue
+++ src/components/HelloWordle.vue
@@ -3,6 +3,11 @@
 /*--------------------------------------------------------------------------------------------
   Imports and variables
 -------------------------------------------------------------------*/
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { ref, defineProps, computed, withDefaults, Ref } from "vue"
 const userWords: Ref<string[]> = ref(["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]);
 const letterColor: Ref<string[]> = ref(["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]);
@@ -12,6 +17,59 @@ type TimerProp = {
   startLabel: string,
   faceColor: string
 }
+
+//FIREBASE STUFF
+/******************************************************************************************************/
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAC0VT8m3ZspBjn-5gcXp1Rck1hq_c0lu4",
+  authDomain: "wordle-snoap-taylor.firebaseapp.com",
+  projectId: "wordle-snoap-taylor",
+  storageBucket: "wordle-snoap-taylor.appspot.com",
+  messagingSenderId: "1064048946619",
+  appId: "1:1064048946619:web:2d319ef9b72845c96de89e",
+  measurementId: "G-1XQ3ERZCYV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
+//example
+const email = "user@example.com";
+const password = "password123";
+
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+
+  createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+
+/********************************************************************************************************/
 
 const props = withDefaults(defineProps<TimerProp>(), {
     updateInterval: 1000,
